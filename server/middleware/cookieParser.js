c54b64a0a87153a4a6ea58b89c021a5d97968010
andrewpiao1 <andrew.piao1@gmail.com>
2019-05-04 20:48:46 -0400

    parseCookies passed all tests

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,27 @@
 const parseCookies = (req, res, next) => {
+  console.log('@    @   parseCookies running @    @   ')
+
+
+  console.log("HEADERS: ", req.headers)
+  console.log("COOKIES property: ", req.headers.cookie)
+
+  if (req.headers.cookie === undefined){
+     req.cookies = {};
+  }else{
+  var cookieString = req.headers.cookie;
+  var cookiesArr = cookieString.split(";");
+  var outputHeaders = {};
+   for(var cookie of cookiesArr) {
+     var arr = cookie.split("=")
+     cookie = arr[1]
+     var key = arr[0].trim()
+     outputHeaders[key] = cookie;
+     console.log("- - - - - - -THIS HEADER IS: ", outputHeaders);
+   }
+   req.cookies = outputHeaders;
+   console.log('@@@@@@', )
+  }
+  next();
 };
 
 module.exports = parseCookies;
